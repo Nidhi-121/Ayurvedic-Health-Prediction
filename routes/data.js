e50d5c952ae5411f75eb1059e3517b08a6ca35b9
Nidhi-121 <114932676+Nidhi-121@users.noreply.github.com>
2024-04-24 12:40:37 +0530

    Added Routes and basic setup

---
+++ routes/data.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const { verifyToken } = require("../middleware/auth");
+const dataControllers = require("../controllers/dataControllers")
+
+const router = express.Router();
+
+router.post("/", verifyToken, dataControllers.createPrediction);
+router.post("/feed", dataControllers.feedData);
+router.get("/history/:id", verifyToken, dataControllers.userHistory);
+router.post("/get", verifyToken, dataControllers.getFormRes);
+
+
+// router.get("/:id", optionallyVerifyToken,dataControllers.getDisease);
+
+
+
+router.delete("/historydelete/:id", verifyToken, dataControllers.userHistorydelete)
+
+module.exports = router;
